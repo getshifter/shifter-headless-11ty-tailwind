79eab1b6d5d628d32f66271f6f29a639e9c71397
Daniel Olson <1872327+emaildano@users.noreply.github.com>
2023-07-13 14:49:49 -0400

    Updated path

--- src/_data/wordpress.js
+++ src/_data/wordpress.js
@@ -1,7 +1,7 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-  let url = `${process.env.WORDPRESS_URL}`;
+  let url = `${process.env.WORDPRESS_URL}/posts`;
 
   /* This returns a promise */
   return EleventyFetch(url, {
