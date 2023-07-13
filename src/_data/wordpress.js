ca6c53a6bd2cccb4e9b77b8b919638171e0c4818
Daniel Olson <1872327+emaildano@users.noreply.github.com>
2023-07-13 14:22:47 -0400

    first commit

---
+++ src/_data/wordpress.js
@@ -0,0 +1,11 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function () {
+  let url = `${process.env.WORDPRESS_URL}`;
+
+  /* This returns a promise */
+  return EleventyFetch(url, {
+    duration: "1d",
+    type: "json",
+  });
+};
